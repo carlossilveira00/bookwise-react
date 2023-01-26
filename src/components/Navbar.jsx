373b232ac841a6ce6ad65940b8e67d5b848a44a2
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-26 22:07:53 -0100

    Create Search Bar component and import it into the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { AppBar, Button, Stack, Toolbar, Typography, IconButton, Box } from '@mui/material'
+import { AppBar, Button, Stack, Toolbar} from '@mui/material'
 import logo from '../Rero-8.svg'
+import { SearchBar } from './SearchBar';
 
 
 const Navbar = () => {
@@ -7,6 +8,7 @@ const Navbar = () => {
     <AppBar position='static'>
       <Toolbar>
           <img src={logo} alt="" width={"100px"}/>
+          <SearchBar placeholder={'Search for Title, Author or ISBN'} />
         <Stack direction="row" spacing={2} sx={{marginLeft: "auto"}}>
           <Button color='inherit' variant='outlined'>Login</Button>
           <Button color='inherit' variant='outlined'>Sign Up</Button>
