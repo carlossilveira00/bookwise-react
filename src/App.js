75f796fc1baba8969ea65470ff3cf0ac011acc93
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-27 09:29:04 -0100

    Create theme and use it to define the colors of the navbar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FBA92B'
+    },
+    secondary:{
+      main: '#565656',
+      light: '9E9E9E'
+    }
+  }
+})
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar/>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { ThemeContext } from '@emotion/react';
 import { AppBar, Button, Stack, Toolbar} from '@mui/material'
 import logo from '../Rero-8.svg'
 import { SearchBar } from './SearchBar';
@@ -5,13 +6,13 @@ import { SearchBar } from './SearchBar';
 
 const Navbar = () => {
   return (
-    <AppBar position='static'>
+    <AppBar position='static' color='default'>
       <Toolbar>
           <img src={logo} alt="" width={"100px"}/>
           <SearchBar placeholder={'Search for Title, Author or ISBN'} />
         <Stack direction="row" spacing={2} sx={{marginLeft: "auto"}}>
-          <Button color='inherit' variant='outlined'>Login</Button>
-          <Button color='inherit' variant='outlined'>Sign Up</Button>
+          <Button color='primary' variant='contained'>Login</Button>
+          <Button color='secondary' variant='contained'>Sign Up</Button>
         </Stack>
       </Toolbar>
     </AppBar>
