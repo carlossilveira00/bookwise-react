b29b4ec4f7672614ce1e88be592f07dae02712f5
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-27 10:59:57 -0100

    Add toggle button to change between themes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FBA92B'
-    },
-    secondary:{
-      main: '#565656',
-      light: '9E9E9E'
+const designTheme = (colorMode) => {
+  return {
+    palette: {
+      mode: colorMode,
+      primary: {
+        main: '#FBA92B'
+      },
+      secondary:{
+        main: '#565656',
+        light: '9E9E9E'
+      }
     }
   }
-})
-
+}
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const theme = createTheme(designTheme(mode))
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme} >
       <div className="App">
-        <Navbar/>
+        <Navbar colorMode={mode} setColorMode={setMode}/>
       </div>
     </ThemeProvider>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import { ThemeContext } from '@emotion/react';
-import { AppBar, Button, Stack, Toolbar} from '@mui/material'
-import logo from '../Rero-8.svg'
+import { AppBar, Button, Stack, ToggleButton, Toolbar} from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import logo from '../Rero-8.svg';
 import { SearchBar } from './SearchBar';
 
 
-const Navbar = () => {
+const Navbar = ({colorMode, setColorMode}) => {
+  const handleColorModeChange = () => {
+    colorMode === 'light' ? setColorMode('dark') : setColorMode('light')
+  };
+
   return (
     <AppBar position='static' color='default'>
       <Toolbar>
           <img src={logo} alt="" width={"100px"}/>
           <SearchBar placeholder={'Search for Title, Author or ISBN'} />
+          <ToggleButton value={colorMode} onChange={handleColorModeChange}>
+            <Brightness4Icon></Brightness4Icon>
+          </ToggleButton>
         <Stack direction="row" spacing={2} sx={{marginLeft: "auto"}}>
           <Button color='primary' variant='contained'>Login</Button>
           <Button color='secondary' variant='contained'>Sign Up</Button>
