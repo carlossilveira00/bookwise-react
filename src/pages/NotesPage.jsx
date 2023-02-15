a8a7235b78f298e3fcb031a474e51f4c317232b8
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-02-15 16:16:53 -0100

    Create structure for the Show Notes component and the notes component.

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -15,12 +15,12 @@ const NotesPage = ({colorMode, setColorMode}) => {
       <Box open={open}
         sx={{
           flexGrow: 1,
-          padding: theme.spacing(3),
+          // padding: theme.spacing(3),
           transition: theme.transitions.create('margin', {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
           }),
-          marginLeft: `-240px`,
+          marginLeft: `-300px`,
           ...(open && {
             transition: theme.transitions.create('margin', {
               easing: theme.transitions.easing.easeOut,
@@ -30,33 +30,48 @@ const NotesPage = ({colorMode, setColorMode}) => {
           }),
         }}
       >
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Box display={'flex'}>
+          <Box sx={{border: '1px solid red', height: '100vh', width: '20%', textAlign:'center'}}>
+            <Typography variant="overline" fontSize={11} color="initial">Notes</Typography>
+            <Box padding={1}>
+              <Stack onClick={() => {console.log('Hey')}} sx={{ boxShadow: 1, textAlign: 'left', whiteSpace: 'nowrap', padding: 2}} >
+                <Typography variant="body1" fontSize={18} color="initial">The Compound Effect</Typography>
+                <Typography variant="body2" fontSize={12} color="initial" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', ml: 1}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</Typography>
+                <Typography variant="body1" fontSize={11} color="initial" sx={{textAlign: 'right', mt: 1}}>01/01/2022</Typography>
+              </Stack>
+            </Box>
+          </Box>
+          <Box sx={{border: '1px solid red', height: '100vh', width: 'fill-available'}}>
+            <Typography paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                sapien faucibus et molestie ac.
+              </Typography>
+              <Typography paragraph>
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+                eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+                neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+                tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+                sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+                gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+                tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+                posuere sollicitudin aliquam ultrices sagittis orci a.
+              </Typography>
+          </Box>
+        </Box>
+
       </Box>
       {/* <Box sx={{border: '1px solid red', height: '100vh', width: '20%'}} textAlign={'center'} paddingTop={1}>
         <Typography variant="overline" color="initial" fontSize={10}>Books</Typography>
