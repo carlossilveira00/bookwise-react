633d0994e1c61cbb3a1a54060c890c9889d772f7
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-02-16 16:12:50 -0100

    Add Trix Text Editor

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,15 +1,23 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { Stack } from "@mui/system";
 import { useTheme } from "@emotion/react";
 import SideBar from "../components/SideBar";
 import { useState } from "react";
 import Note from "../components/Note";
 import ShowNotes from "../components/ShowNotes";
+import Trix from "trix";
+import { ReactTrixRTEInput, ReactTrixRTEToolbar } from "react-trix-rte";
 
 
 const NotesPage = ({colorMode, setColorMode}) => {
   const theme = useTheme();
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  function handleChange(event, newValue) {
+    setValue(newValue); // OR custom on change listener.
+  }
+
 
   return (
     <>
@@ -39,34 +47,12 @@ const NotesPage = ({colorMode, setColorMode}) => {
             <Note></Note>
             <Note></Note>
           </ShowNotes>
-          <Box sx={{border: '1px solid red', height: '100vh', width: 'fill-available'}}>
-            <Typography paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                sapien faucibus et molestie ac.
-              </Typography>
-              <Typography paragraph>
-                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                posuere sollicitudin aliquam ultrices sagittis orci a.
-              </Typography>
+          <Box sx={{border: '1px solid red', height: '100vh', width: 'fill-available', padding: 3}}>
+            <ReactTrixRTEInput
+                defaultValue="<div>React Trix Rich Text Editor</div>"
+                onChange={handleChange}
+                className={'text-editor'}
+              />
           </Box>
         </Box>
 
